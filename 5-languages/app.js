75789f88ab7920a07cce98527c23603965f1d7db
saloon <danilmilyutin2000@gmail.com>
2024-03-18 18:47:24 +0300

    commit

---
+++ 5-languages/app.js
@@ -0,0 +1,23 @@
+const language = 'ru';
+
+switch (language) {
+    case 'en':
+    console.log('Hello, Dev!');
+    break;
+
+    case 'ru':
+    console.log('Привет, разработчик!');
+    break;
+
+    case 'ua':
+    console.log('Привіт, розробнику!');
+    break;
+
+    case 'de':
+    console.log('Hallo, Entwickler!');
+    break;
+
+    default:
+    console.log('Hello, Dev!');
+};
+
